aosp-mirror/platform_external_qemu

[video] goldfish_dma androidemu changes
This set of CLs instantiates the goldfish dma
pipe extension's interface, and uses it to
perform all color buffer updates, making
video playback smoother.

Change-Id: I543ba907fcc948d48d812146fa2e69a86cecb941

distrib/android-emugl/host/libs/libOpenglRender/RenderLibImpl.h
@@ -35,6 +35,7 @@ public:
                                emugl_sync_register_trigger_wait_t,
                                emugl_sync_device_exists_t) override;
 
+    virtual void setDmaGetter(emugl_dma_get_t) override;
     virtual RendererPtr initRenderer(int width,
                                      int height,
                                      bool useSubWindow) override;

andocker/libandroid-emugl

host/libs/libOpenglRender/RenderLibImpl.h
@@ -35,6 +35,8 @@ public:
                                emugl_sync_register_trigger_wait_t,
                                emugl_sync_device_exists_t) override;
 
+    virtual void setDmaOps(emugl_dma_ops) override;
+
     virtual RendererPtr initRenderer(int width,
                                      int height,
                                      bool useSubWindow) override;
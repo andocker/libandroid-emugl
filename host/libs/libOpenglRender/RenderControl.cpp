aosp-mirror/platform_external_qemu

[driver-bug-workaround] Don't resize until guest posted
It's possible for the TextureResize resize() to cause a crash
on snapshot load on NVIDIA GPUs.

We haven't seen this before in testing probably because we tend to
run on high resolution monitors or without the device frame. With
the device frame, it's more likely that a nontrivial resize needs
to be applied to the emulator.

Change-Id: I1c9f784038c0319bebf796e3a7b2ddc1519a0b16

android/android-emugl/host/libs/libOpenglRender/RenderControl.cpp
@@ -632,6 +632,7 @@ static void rcFBPost(uint32_t colorBuffer)
     }
 
     fb->post(colorBuffer);
+    fb->setGuestPosted();
 }
 
 static void rcFBSetSwapInterval(EGLint interval)

android/android-emugl/host/libs/libOpenglRender/TextureResize.cpp
@@ -292,8 +292,13 @@ GLuint TextureResize::update(GLuint texture) {
         i++, w /= 2, h /= 2, factor *= 2) {
     }
 
-    // No resizing needed.
-    if (factor == 1) {
+    // No resizing needed if factor == 1, or if
+    // the guest has not posted a frame yet. We wait for the guest
+    // to post a frame because on snapshot load with NVIDIA GPU's,
+    // we get a crash in the subsequent glDrawArrays in resize()
+    // unless we wait for more initiaalization to have taken place.
+    if (factor == 1 ||
+        !FrameBuffer::getFB()->hasGuestPosted()) {
         return texture;
     }
 

andocker/libandroid-emugl

bug: 68040469

host/libs/libOpenglRender/RenderControl.cpp
@@ -632,6 +632,7 @@ static void rcFBPost(uint32_t colorBuffer)
     }
 
     fb->post(colorBuffer);
+    fb->setGuestPostedAFrame();
 }
 
 static void rcFBSetSwapInterval(EGLint interval)

host/libs/libOpenglRender/TextureResize.cpp
@@ -292,8 +292,13 @@ GLuint TextureResize::update(GLuint texture) {
         i++, w /= 2, h /= 2, factor *= 2) {
     }
 
-    // No resizing needed.
-    if (factor == 1) {
+    // No resizing needed if factor == 1, or if
+    // the guest has not posted a frame yet. We wait for the guest
+    // to post a frame because on snapshot load with NVIDIA GPU's,
+    // we get a crash in the subsequent glDrawArrays in resize()
+    // unless we wait for more initialization to have taken place.
+    if (factor == 1 ||
+        !FrameBuffer::getFB()->hasGuestPostedAFrame()) {
         return texture;
     }
 